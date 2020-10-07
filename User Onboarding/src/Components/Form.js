ad669096eb8609bad73abf98f2cec28c7aeb8048
ChadDiaz <chadcdiaz@gmail.com>
2020-10-07 00:56:52 -0400

    mvp met

--- User Onboarding/src/Components/Form.js
+++ User Onboarding/src/Components/Form.js
@@ -8,36 +8,72 @@ const Form = () => {
         name: '',
         email: '',
         password: '',
-        terms: false
+        terms: false,
     })
 
     //Button state
     const[buttonOff, setButtonOff]=useState(true)
-    
-    //setPost State
-    const[post, setPost] = useState([])
-    
+      
+    //Errors State
+    const [errors, setErrors]=useState({
+        name: '',
+        email: '',
+        password: '',
+        terms: '', 
+    })
+
+
     
     //onChange function
     const inputChange = (e) => {
+        e.persist();
+        lineSchema(e)
         setUser({...user, [e.target.name]: e.target.type==='checkbox' ? e.target.checked : e.target.value})
     }
-    
+
+     //setPost State - just to have the display come out at the bottom.  Not really needed.
+    const[post, setPost] = useState([])   
+
     //onSubmit function
     const formSubmit = (e) => {
         e.preventDefault()
         axios.post("https://reqres.in/api/users" , user)
         .then(response => {
             console.log("response", response)
-            setPost(response.data)
+            setPost(response.data)// again, not really needed
             setUser({
                 name: '',
                 email: '',
                 password: '',
-                terms: false
+                terms: false,
             })
         }).catch((err) => console.log(err.response))
-    }   
+    }  
+    
+    //  all yup coding below
+    const schema = yup.object().shape({
+        name: yup.string().required("Name is a required field"),
+        email: yup.string("Must be a valid email").required("Must include email address"),
+        password: yup.string().min(5).max(10).required("Must have a valid password"),
+        terms: yup.boolean().oneOf([true], "Please agree to T&Cs")
+    })
+
+    useEffect(() => {
+        schema.isValid(user).then((valid) => {
+            setButtonOff(!valid)
+        })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user])
+
+    const lineSchema = (e) => {
+        yup.reach(schema, e.target.name)
+        .validate(e.target.type==='checkbox' ? e.target.checked : e.target.value)
+        .then((valid) => {
+            setErrors({...errors, [e.target.name]: ''})
+        }).catch((err) => {
+            setErrors({...errors, [e.target.name]: err.errors[0]})
+        })
+    }
     
 
     console.log('userState' , user)
@@ -50,6 +86,7 @@ const Form = () => {
                     name='name'
                     value={user.name}
                     onChange={inputChange}
+                    placeholder="Enter First & Last Name"
                 />
             </label>
             <label htmlFor='email'>Email
@@ -59,6 +96,7 @@ const Form = () => {
                     name='email'
                     value={user.email}
                     onChange={inputChange}
+                    placeholder="Valid Email Address"
                 />
             </label>
             <label htmlFor='password'>Password
@@ -68,6 +106,7 @@ const Form = () => {
                     name='password'
                     value={user.password}
                     onChange={inputChange}
+                    placeholder="min 5 / max 10"
                 />
             </label>
             <label htmlFor='terms' className="terms">
@@ -79,7 +118,7 @@ const Form = () => {
                     onChange={inputChange}
                 />Terms & Conditions
             </label>
-            <button type="submit">
+            <button type="submit" disabled={buttonOff}>
                 Submit
             </button>
             <pre>{JSON.stringify(post, null, 2)}</pre>

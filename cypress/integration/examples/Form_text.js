598b3da4aab947c2cc113ac1914159e6c57a91ec
ChadDiaz <chadcdiaz@gmail.com>
2020-10-09 23:30:32 -0400

    finished texting

--- cypress/integration/examples/Form_text.js
+++ cypress/integration/examples/Form_text.js
@@ -3,6 +3,10 @@ describe('texting our form inputs', () => {
         cy.visit('http://localhost:3000')
     })
     it("add texts to input and submits form", () => {
-        cy.get('[data-cy=name]').type("Chad").should("have.value" ,"Chad")
+        cy.get('[data-cy=name]').type("Chad").should("have.value" ,"Chad");
+        cy.get('[data-cy=email]').type("chad@gmail.com").should('have.value', 'chad@gmail.com');
+        cy.get('[data-cy=password]').type('password').should('have.value','password');
+        cy.get('[data-cy=terms]').check().should('be.checked')
+        cy.get('[data-cy=submit]').click()
     })
 });
